43ad6ccfe042734503d66a3f2ec83ae9c5960476
Yannis OURAGHI <yannis.ouraghi@viacesi.fr>
2023-08-02 15:29:05 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using APIAnnuaire.Models;
+
+namespace APIAnnuaire.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly List<Employee> _employees;
+
+        public EmployeeController()
+        {
+            string xmlFilePath = "EmployeeData.xml";
+            _employees = EmployeeData.LoadDataFromXml(xmlFilePath); // Mettez à jour la liste _employees avec les données chargées depuis le fichier XML
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return _employees.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // Autres méthodes d'action pour les opérations POST, PUT, DELETE, etc.
+    }
+}
---
+++ EmployeeData.cs
@@ -0,0 +1,35 @@
+﻿using APIAnnuaire.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace APIAnnuaire
+{
+    public static class EmployeeData
+    {
+        public static List<Employee> LoadDataFromXml(string xmlFilePath)
+        {
+            XDocument xmlDoc = XDocument.Load(xmlFilePath);
+            List<Employee> employees = xmlDoc.Root.Elements("table")
+                .Where(table => table.Attribute("name")?.Value == "employees")
+                .Elements("row")
+                .Select(row => new Employee
+                {
+                    Id = int.Parse(row.Element("Id").Value),
+                    FirstName = row.Element("FirstName").Value,
+                    LastName = row.Element("LastName").Value,
+                    Department = row.Element("Department").Value,
+                    Email = row.Element("Email").Value,
+                    PhoneNumber = row.Element("PhoneNumber").Value,
+                    MobilePhone = row.Element("MobilePhone").Value,
+                    JobTitle = row.Element("JobTitle").Value,
+                    JobDescription = row.Element("JobDescription").Value,
+                    Service = row.Element("Service").Value,
+                    Site = row.Element("Site").Value
+                })
+                .ToList();
+
+            return employees;
+        }
+    }
+}
