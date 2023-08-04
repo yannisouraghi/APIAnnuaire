ac7018352a749b3bdbe0547db1100ed449dcf74e
Yannis OURAGHI <yannis.ouraghi@viacesi.fr>
2023-08-04 10:43:09 +0200

    Intégration de l'API au projet Annuaire

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -35,6 +35,43 @@ namespace APIAnnuaire.Controllers
             return employee;
         }
 
-        // Autres méthodes d'action pour les opérations POST, PUT, DELETE, etc.
+        [HttpGet("site/{Site}")]
+        public ActionResult<List<Employee>> GetEmployeesBySite(string Site)
+        {
+            var employeesBySite = _employees.Where(e => e.Site.Equals(Site, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (employeesBySite.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return employeesBySite;
+        }
+
+        [HttpGet("service/{Service}")]
+        public ActionResult<List<Employee>> GetEmployeesByService(string Service)
+        {
+            var employeesByService = _employees.Where(e => e.Service.Equals(Service, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (employeesByService.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return employeesByService;
+        }
+
+        [HttpGet("lastname/{LastName}")]
+        public ActionResult<List<Employee>> GetEmployeesByLastName(string LastName)
+        {
+            var employeesByLastName = _employees.Where(e => e.LastName.Equals(LastName, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (employeesByLastName.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return employeesByLastName;
+        }
     }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,8 +7,8 @@
         public string LastName { get; set; }
         public string Department { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string MobilePhone { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; }
+        public string MobilePhone { get; set; }
         public string JobTitle { get; set; }
         public string JobDescription { get; set; }
         public string Service { get; set; }
